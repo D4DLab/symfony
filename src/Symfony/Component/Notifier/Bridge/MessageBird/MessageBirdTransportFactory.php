72308335701bf4f3123248bc48cc5a510d8ee20a
Vasilij Dusko | CREATION <vasilij@creation.lt>
2021-03-30 08:25:27 +0300

    * message bird quick start

---
+++ src/Symfony/Component/Notifier/Bridge/MessageBird/MessageBirdTransportFactory.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Notifier\Bridge\MessageBird;
+
+use Symfony\Component\Notifier\Exception\UnsupportedSchemeException;
+use Symfony\Component\Notifier\Transport\AbstractTransportFactory;
+use Symfony\Component\Notifier\Transport\Dsn;
+use Symfony\Component\Notifier\Transport\TransportInterface;
+
+/**
+ * @author Vasilij Duško <vasilij@prado.lt>
+ */
+final class MessageBirdTransportFactory extends AbstractTransportFactory
+{
+    /**
+     * @return MessageBirdTransport
+     */
+    public function create(Dsn $dsn): TransportInterface
+    {
+        $scheme = $dsn->getScheme();
+
+        if ('messagebird' !== $scheme) {
+            throw new UnsupportedSchemeException($dsn, 'messagebird', $this->getSupportedSchemes());
+        }
+
+        $accountSid = $this->getUser($dsn);
+        $authToken = $this->getPassword($dsn);
+        $from = $dsn->getRequiredOption('from');
+        $host = 'default' === $dsn->getHost() ? null : $dsn->getHost();
+        $port = $dsn->getPort();
+
+        return (new MessageBirdTransport($accountSid, $authToken, $from, $this->client, $this->dispatcher))->setHost($host)->setPort($port);
+    }
+
+    protected function getSupportedSchemes(): array
+    {
+        return ['messagebird'];
+    }
+}
